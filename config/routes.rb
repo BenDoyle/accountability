e17c72b8163c4bd882855fa1aa1f2755c11cfcf6
Ben Doyle <doyle.ben@gmail.com>
2010-11-27 15:13:59 -0500

    updated routes to root app at home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Roundup::Application.routes.draw do
+  
+  resources :home
+
   resources :people_stories
 
   resources :stories
@@ -54,7 +57,7 @@ Roundup::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
