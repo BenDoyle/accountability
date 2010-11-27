fb946e533cea91a5c3dd962f8e9f7939d451cd71
Ben Doyle <doyle.ben@gmail.com>
2010-11-27 15:13:16 -0500

    added views and configured controllers for person,story and home pages

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,9 @@
 class PeopleController < ApplicationController
+  def show
+    @person  = Person.find(params[:id])
+    @stories = @person.stories
+  end
+  def index
+    @people = Person.all
+  end
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,9 @@
 class StoriesController < ApplicationController
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def index
+    @stories = Story.all    
+  end
 end
