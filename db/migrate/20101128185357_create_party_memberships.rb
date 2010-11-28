e9d66d9f0590486aab6c0006fe6d66d933007715
Ben Doyle <doyle.ben@gmail.com>
2010-11-28 14:39:35 -0500

    fixed broken link to party_id

--- db/migrate/20101128185357_create_party_memberships.rb
+++ db/migrate/20101128185357_create_party_memberships.rb
@@ -2,7 +2,7 @@ class CreatePartyMemberships < ActiveRecord::Migration
   def self.up
     create_table :party_memberships do |t|
       t.integer 'person_id'
-      t.integer 'organization_id'
+      t.integer 'party_id'
       t.integer 'role_id'
       t.date 'start_date'
       t.date 'end_date'
